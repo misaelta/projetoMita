a43ed8601cec27e42631f21e3674ea9e84787314
misaelta <=misaelt.a@hotmail.com>
2026-08-12 22:33:59 -0300

    add footer e header

---
+++ pages/header.php
@@ -0,0 +1,20 @@
+<?php
+
+session_start();
+
+$nome =$_SESSION['usuario_nome'];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hortifruti Felix</title>
+</head>
+
+
+<body>
+    
+
--- pages/paginaPrincipal.php
+++ pages/paginaPrincipal.php
@@ -1,26 +1,14 @@
-<?php
-
-session_start();
-
-$nome =$_SESSION['usuario_nome'];
-
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-
-<h1> BEM VINDO! <?= $nome ?> </h1>
-
-</body>
-</html>
+<?php include("header.php") ?>
 
 
+
+
+
+
+
+
+
+
+<?php include("footer.php") ?>
 
 
